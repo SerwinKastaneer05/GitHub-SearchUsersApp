3a52cf8dad42dd23e38a000dd6854b3d2156b3cb
Serwin Kastaneer <98677021+SerwinKastaneer05@users.noreply.github.com>
2022-10-20 17:00:19 -0400

    .

--- src/index.js
+++ src/index.js
@@ -3,15 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { GithubProvider } from './context/context';
+import { GithubContext, GithubProvider } from './context/context';
 import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GithubProvider>
+      <App />
+    </GithubProvider>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
