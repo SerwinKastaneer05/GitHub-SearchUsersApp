67117494d5f6dadaaa66bea0579a83db738c84b5
Serwin Kastaneer <98677021+SerwinKastaneer05@users.noreply.github.com>
2022-12-07 17:00:42 -0500

    .

--- src/components/Info.js
+++ src/components/Info.js
@@ -53,7 +53,7 @@ const UserInfo = () => {
 }
 const Item = ({ icon, label, value, color }) => {
   return (
-    <article>
+    <article className='item'>
       <span className={color}>{icon}</span>
       <div>
         <h3>{value}</h3>
