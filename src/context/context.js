7ff526a0a7bbd90d42d9351f3c275bb22f1967a8
Serwin Kastaneer <98677021+SerwinKastaneer05@users.noreply.github.com>
2022-12-08 16:07:19 -0500

    .

--- src/context/context.js
+++ src/context/context.js
@@ -11,11 +11,41 @@ const GithubContext = React.createContext()
 //Provider, Consumer - GithubContext.Provider
 
 const GithubProvider = ({ children }) => {
-const [githubUser, setGithubUser] =useState(mockUser);
-const [repos, setRepos] = useState(mockRepos);
-const [followers, setfollowers] = useState(mockFollowers);
+  const [githubUser, setGithubUser] = useState(mockUser)
+  const [repos, setRepos] = useState(mockRepos)
+  const [followers, setfollowers] = useState(mockFollowers)
+  //request loading
+  const [requests, setRequests] = useState(0)
+  const [loading, setIsLoading] = useState(false)
+  //error
+  const [error, setError] = useState({ show: false, msg: '' })
+  //check rate
+  const checkRequests = () => {
+    axios(`${rootUrl}/rate_limit`)
+      .then(({ data }) => {
+        let {
+          rate: { remaining },
+        } = data
+
+        setRequests(remaining)
+        if (remaining === 0) {
+          toggleError(true, 'sorry, you have exceeded your hourly rate limit!')
+        }
+      })
+      .catch((err) => console.log(err))
+  }
+  function toggleError(show = false, msg = '') {
+    setError({ show, msg })
+  }
+  //error
+  useEffect(checkRequests, [])
+
   return (
-    <GithubContext.Provider value={{githubUser,repos,followers}}>{children}</GithubContext.Provider>
+    <GithubContext.Provider
+      value={{ githubUser, repos, followers, requests, error }}
+    >
+      {children}
+    </GithubContext.Provider>
   )
 }
 
